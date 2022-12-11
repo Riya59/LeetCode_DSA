b4f4d9df24a28880c1d6bbe3b126a2384435658e
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-11 21:54:54 +0530

    Update first_last_position.java

--- first_last_position.java
+++ first_last_position.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
+        Arrays.sort(nums);
         int first = -1, last = -1;
         for(int i=0;i<n;i++){
             if(first == -1 && nums[i] == target){
