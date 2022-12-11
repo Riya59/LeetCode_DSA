7cfcaadf7157181025cff1b0c3a8b973dffdc327
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-11 21:54:30 +0530

    Create first_last_position.java

---
+++ first_last_position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int first = -1, last = -1;
+        for(int i=0;i<n;i++){
+            if(first == -1 && nums[i] == target){
+                first = i;
+            }
+           if(nums[i] == target){
+                last = i;
+            }
+        }
+        int index[] = {first,last};
+        return index;
+    }
+}
