c3590cff77bc0837406d9d7c2d3a19321e7a3308
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-14 16:06:20 +0530

    Create House_Robber.java

---
+++ House_Robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int incl = nums[0];
+        int excl_new;
+        int i;
+        for(i=1;i<n;i++){
+            excl_new = Math.max(incl, sum);
+            incl = sum + nums[i];
+            sum = excl_new;
+        }
+        return Math.max(incl,sum);
+    }
+}
