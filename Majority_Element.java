3a6e9f861ffd08c22301eb04980b613d0a75e2c5
Riya Verma <77240974+Riya59@users.noreply.github.com>
2022-12-29 18:57:09 +0530

    Create Majority_Element.java

---
+++ Majority_Element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int ret = 0;
+        for(int num:nums){
+            if(count == 0){
+                ret = num;
+            }
+            if(num!=ret){
+                count--;
+            }
+            else{
+                count++;
+            }
+        }
+        return ret;
+    }
+}
